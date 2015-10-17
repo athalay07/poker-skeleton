33e7cedf1e0160aa8742596a30d78c3a767e245f
athalay07 <athalay2007@gmail.com>
2015-10-17 12:54:52 +0200

    same suit

--- src/main/java/com/wcs/poker/bloodorange/evaluationphase/HandEvaluationPhase.java
+++ src/main/java/com/wcs/poker/bloodorange/evaluationphase/HandEvaluationPhase.java
@@ -5,6 +5,7 @@
  */
 package com.wcs.poker.bloodorange.evaluationphase;
 
+import com.wcs.poker.gamestate.Card;
 import com.wcs.poker.gamestate.GameState;
 import com.wcs.poker.gamestate.Player;
 import com.wcs.poker.gamestate.Rank;
@@ -30,16 +31,29 @@ public class HandEvaluationPhase implements EvaluationPhase {
     @Override
     public int eval(GameState gameState) {
         Player player = gameState.getPlayers().get(gameState.getInAction());
-        Rank rank1 = player.getHoleCards().get(0).getRank();
-        Rank rank2 = player.getHoleCards().get(1).getRank();
+        Card card1 = player.getHoleCards().get(0);
+        Rank rank1 = card1.getRank();
+        Card card2 = player.getHoleCards().get(1);
+        Rank rank2 = card2.getRank();
+
+        boolean sameSuit = card1.getSuit().equals(card2.getSuit());
+
         int value = 0;
 
         if (highRanks.contains(rank1) || highRanks.contains(rank2)) {
             value = 2;
         }
 
-        if (highRanks.contains(rank1) && highRanks.contains(rank2)) {
+        if (highRanks.contains(rank1) && highRanks.contains(rank2) && sameSuit) {
             value = 4;
+        }
+
+        if (highRanks.contains(rank1) && highRanks.contains(rank2)) {
+            value = 6;
+        }
+
+        if (highRanks.contains(rank1) && highRanks.contains(rank2) && sameSuit) {
+            value = 8;
         }
 
         if (rank1.equals(rank2)) {
