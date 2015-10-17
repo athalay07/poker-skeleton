76edab13a72ffeb18014aaf8f377b31eb3c6f3ee
Timi <fe.timea@gmail.com>
2015-10-17 10:34:01 +0200

    ideiglenes stratégia

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -7,6 +7,10 @@ public class Player {
     static final String VERSION = "BloodOrange";
 
     public static int betRequest(GameState gameState) {
+        com.wcs.poker.gamestate.Player player = gameState.getPlayers().get(gameState.getInAction());
+        if (player.getHoleCards().get(0).getRank().equals(player.getHoleCards().get(1).getRank())) {
+            return gameState.getCurrentBuyIn() - player.getBet() + gameState.getMinimumRaise();
+        }
 
         return 0;
     }
